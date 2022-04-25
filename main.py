230265a42c6bf804080198d61d69f835eeebc67c
AJ <91902546+DelMonteAJ@users.noreply.github.com>
2022-04-25 14:32:06 -0500

    Added v.1.0.2b

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from re import T
+from shlex import split
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 import codecs
 from math import ceil
 import os
@@ -54,33 +57,133 @@ def ripper(driver, path):
     f.close()
     print("[+] Finished ripping")
     os.remove(n)
+
+# def dependencyRun(driver, path):
+#     reader = codecs.open(path + "\\dependencies.txt", "r", "utf-8")
+#     lines = reader.readlines()
+#     reader.close()
+#     writer = open(path +"\\depend_out.txt", "w")
+
+#     count = 0
+#     links = []
+#     for line in lines:
+#         count += 1
+#         if "https://steamcommunity.com/workshop/filedetails" in line and "filedetails/discussion" not in line:
+
+#             sindex = line.find("https://steamcommunity.com/workshop/filedetails")
+#             eindex = line[sindex:].find("target") + sindex
+#             link = line[sindex:eindex-2]
+#             if link not in links:
+#                 links.append(link)
+#                 writer.line(link)
+#                 subscribe(driver, link)
+#             continue
+
+#     print(links)
+#     writer.close()
+
+
+def dependency(driver, path):
+    reader = codecs.open(path + "\\depend.txt", "r", "utf-8")
+    lines = reader.readlines()
+    reader.close()
+    count = 0
+    links = []
+    for line in lines:
+        count += 1
+        if "https://steamcommunity.com/workshop/filedetails" in line and "filedetails/discussion" not in line:
+
+            sindex = line.find("https://steamcommunity.com/workshop/filedetails")
+            eindex = line[sindex:].find("target") + sindex
+            link = line[sindex:eindex-2]
+            if link not in links:
+                links.append(link)
+                subscribe(driver, link, path)
+            continue
+
+def subscribe(driver, link, path):
+    splitPath = path
+    driver.get(link)
+    try:
+        element = driver.find_element(by=By.CLASS_NAME, value= "subscribeText")
+        if "Subscribed" in element.text:
+            print("[+] Already subscribed to " + link + "\n\n")
+        else:
+            print("[+] Subscribed to " + link + "\n\n")
+            driver.find_element(by=By.ID, value= "SubscribeItemBtn").click()
+            try:
+                newE = driver.find_element(by=By.CLASS_NAME, value= "requiredItemsContainer")
+                print("[-] Error: This workshop item has dependencies! " + link + "\n")
+                with codecs.open(splitPath + "\\depend.txt", "w", "utf-8") as dWriter:
+                    dWriter.write(driver.page_source)
+                dependency(driver,splitPath)
+                driver.get(link)
+                if driver.find_element(by=By.CLASS_NAME, value= "subscribeText").text != "Unsubscribe" and driver.find_element(by=By.CLASS_NAME, value= "subscribeText").text != "Subscribed":
+                        driver.find_element(by=By.ID, value= "SubscribeItemBtn").click()
+            except:
+                pass
+    except Exception as e:
+        print("[-] Error: This workshop may be unavailable! " + link + "\n")
+
+
 def subscriber(driver, path):
 
     print("\n")
 
     path = path + "\\links\\links.txt"
 
-    reader = open(path, "r")
+    try:
+        reader = open(path, "r")
+    except FileNotFoundError:
+        print("[-] links.txt cannot be found.\n")
+        return
     lines = reader.readlines()
     reader.close()
-    errorWriter = open(path.split("\\links.txt")[0] + "\\error.txt", "w")
+    splitPath = path.split("\\links.txt")[0]
+    errorWriter = open(splitPath + "\\error.txt", "w")
+    dependWriter = codecs.open(splitPath + "\\dependencies.txt", "w", "utf-8")
     count = 0
     for line in lines:
         count += 1 
         link = line.split(" - ")[0]
+        title = line.split(" - ")[1]
         driver.get(link)
         try:
             element = driver.find_element(by=By.CLASS_NAME, value= "subscribeText")
             if "Subscribed" in element.text:
                 print("[+] Already subscribed to " + link + "["+ str(count) + "/" + str(len(lines)) + "]\n\n")
             else:
-                print("[+] Subscribed to " + link + "["+ str(count) + "/" + str(len(lines)) + "]\n\n")
+                
                 driver.find_element(by=By.ID, value= "SubscribeItemBtn").click()
+                try:
+                    newE = driver.find_element(by=By.CLASS_NAME, value= "requiredItemsContainer")
+                    print("[-] Error: This workshop item has dependencies! " + line.split(" - ")[1] + " - " + link + "\n")
+                    errorWriter.write(link + " - " + title + " - Has dependencies!\n")
+                    with codecs.open(splitPath + "\\depend.txt", "w", "utf-8") as dWriter:
+                        dWriter.write(driver.page_source)
+                    dependency(driver,splitPath)
+                    driver.get(link)
+                    
+                    if driver.find_element(by=By.CLASS_NAME, value= "subscribeText").text != "Unsubscribe" and driver.find_element(by=By.CLASS_NAME, value= "subscribeText").text != "Subscribed":
+                        driver.find_element(by=By.ID, value= "SubscribeItemBtn").click()
+                    
+                    
+                    if driver.find_element(by=By.CLASS_NAME, value= "subscribeText").text == "Subscribe":
+                        driver.find_element(by=By.ID, value= "SubscribeItemBtn").click()
+                    print("[+] Subscribed to " + link + "["+ str(count) + "/" + str(len(lines)) + "]\n\n")
+                    
+
+                    # dependWriter.write(driver.page_source)
+                except:
+                    print("[+] Subscribed to " + link + "["+ str(count) + "/" + str(len(lines)) + "]\n\n")
+                    pass
         except Exception as e:
             print("[-] Error: This workshop may be unavailable! " + line.split(" - ")[1] + " - " + link + "\n")
-            errorWriter.write(line) 
-
+            errorWriter.write(line + " - Not avaiable!") 
+    dependWriter.close()
+    # dependencyRun(driver, splitPath)
     print("[+] Done")
+    
     errorWriter.close()
 
 running = True
@@ -101,12 +204,12 @@ driver.get("https://steamcommunity.com/login/home/?goto=")
 #         annoyCheck = True
 #         print("[+] Waiting for login page to load")
 #     sleep(0.1)
-annoyCheck = False
-while "login" in driver.current_url:
-    if not annoyCheck:
-        annoyCheck = True
-        print("[+] Waiting for successful login")
-    sleep(0.1)
+# annoyCheck = False
+# while "login" in driver.current_url:
+#     if not annoyCheck:
+#         annoyCheck = True
+#         print("[+] Waiting for successful login")
+#     sleep(0.1)
 print("[+] Logged in")
 
 
